484bbdec18cebdcb07320e08938e4479ff84abc3
Joan Andrés Vázquez Celi <joan_vazquez@ceroca.cat>
2025-10-30 19:20:16 +0100

    Menu Final
    Menu con fondo y botones funcionales de cambio de escenas

---
+++ Assets/Scripts/CambioDeScenas.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // 👈 NECESARIO para cambiar escenas
+
+public class CambioDeScenas : MonoBehaviour
+{
+  
+  public void LoadMainMenu()
+    {
+        // Carga la escena del menú principal.
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+    public void MainGame()
+    {
+        // Carga la escena del juego principal.
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Partida");
+    }
+    public void Instrucciones()
+    {
+        // Carga la escena del juego principal.
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ComoJugar");
+    }
+}
